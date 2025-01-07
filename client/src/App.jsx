4eead84117de4fa180e18b685b5899821242d79f
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-01-07 10:13:04 +0600

    Create Some Pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,18 @@
+import { Route, Routes } from "react-router-dom"
+import Home from "./pages/Home"
+import Login from "./pages/Login"
+import EmailVerify from "./pages/EmailVerify"
+import ResetPassword from "./pages/ResetPassword"
 
 const App = () => {
   return (
     <div >
-    hello
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
     </div>
   )
 }
