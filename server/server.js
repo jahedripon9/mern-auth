b7416bd324a8d3ff14398fd0c930e9d950b57ad1
Jahed Ahmed Ripon <73092252+jahedripon9@users.noreply.github.com>
2025-01-11 10:52:57 +0600

    update

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ connectDB();
 
 // Enable CORS for specific origin
 app.use(cors({
-    origin: 'https://web-builder-bangladesh-backend.vercel.app',
+    origin: 'https://web-builder-bangladesh-frontend.vercel.app',
     credentials: true, // Allows cookies to be sent
 }));
 
